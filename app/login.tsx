ea0d6f4feab401268e30c83c99b67f648736e4cb
Chasles Maxime <maxime.chasles@etudiant.univ-lr.fr>
2024-10-11 17:38:11 +0200

    add styles

--- app/login.tsx
+++ app/login.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import {  Text, View, TextInput, Button, Alert } from "react-native";
+import {  Text, View, TextInput, Button, StyleSheet } from "react-native";
 import { Link } from 'expo-router';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 /**
  * Identifiants de connexion d'un utilisateur
@@ -35,17 +36,23 @@ export default function Login() {
         alignItems: "center",
       }}
     >
-      <Text>Ecovoit</Text>
-      <Text>Connectez-vous</Text>
+    
+    <View style={styles.header}>
+      <Text style={styles.h1}>Ecovoit</Text>
+      <Text style={styles.h2}>Connectez-vous</Text>
+    </View>
+      
 
       <View>
         <TextInput
+          style={styles.input}
           onChangeText={setUsername}
           value={username}
           placeholder='Email'
           inputMode='email'
         />
         <TextInput
+          style={styles.input}
           onChangeText={setPassword}
           value={password}
           placeholder='Mot de passe'
@@ -55,14 +62,62 @@ export default function Login() {
       </View>
       
 
-      <Button title='Se connecter'
-              onPress={handleSubmit}
-      ></Button>
+      <TouchableOpacity
 
-      <Text >Pas encore membre ?</Text>
+        style={styles.button}
+        onPress={handleSubmit}
+        
+      >
+        <Text style={styles.buttonText}>Se connecter</Text>
 
-      <Link href='/login' >Inscription</Link>
+      </TouchableOpacity>
+
+      <View style={styles.footer}>
+        <Text style={styles.h3}>Pas encore membre ?</Text>
+        <Link href='/login'>Inscription</Link>
+      </View>
+      
 
     </View>
   );
 }
+
+const styles = StyleSheet.create(
+  {
+    header: {
+      justifyContent: "center",
+      alignItems: "center",
+      marginBottom: 40
+    },
+    h1: {
+      fontSize: 40,
+    },
+    h2: {
+      fontSize: 30,
+    },
+    h3: {
+      fontSize: 20
+    },
+    input: {
+      backgroundColor: '#aaa',
+      height: 40,
+      width: 300,
+      marginBottom: 20,
+      borderRadius: 8,
+      padding: 7.5,
+    },
+    button: {
+      backgroundColor: "#2196F3",
+      padding: 10,
+      borderRadius: 7.5,
+      marginBottom: 30
+    },
+    buttonText: {
+      fontSize: 20,
+      color: "#fff",
+    },
+    footer: {
+      width: 300
+    }
+  }
+)
