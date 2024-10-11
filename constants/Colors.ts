1e729efb1161beec1ab9ae60292ef461a466fcfe
Quentin <sautiereq@gmail.com>
2024-10-11 17:29:36 +0200

    add google font module, static title font and color constants

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,24 +3,18 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
-
 export const Colors = {
   light: {
     text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
+    primary: '#00570D',
+    secondary: '#106EB2',
+    accent: '#6CC5D5',
+    background: '#FDFFFD',
+    inputText : '#616161',
+    inputBackground : '#E6E6E6',
+
   },
   dark: {
     text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
   },
 };
