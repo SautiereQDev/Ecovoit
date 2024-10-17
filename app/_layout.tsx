dcf7fb960906cc59858474a212669708af5832cd
Chasles Maxime <maxime.chasles@etudiant.univ-lr.fr>
2024-10-17 15:10:51 +0200

    clean dir

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,39 @@
 import { Stack } from "expo-router";
+import Login from "./login.js";
+import { users } from "@/tests/User.js";
 
 export default function RootLayout() {
+	// const [authenticated, setAuthenticated] = useState(false);
+
+	// POST /user/?email=hfhfghfh&password=hfhgfghvhgv
+	// GET /users
+
+	// 501 -> error -> fuck -> login
+	// 200 -> Token -> Storage -> Home
+
+	// Token ? Home : Login
+
+	// response = {
+	// 	header: "",
+	// 	body: {
+	// 		auth: {
+	// 			token: "vjhghjghvjhvjvj"
+	// 		},
+	// 		data: {
+	// 			username:
+	// 			pssword
+	// 		}
+
+	// 	}
+	// }
+
 	return (
 		<Stack>
+			{/* Utilisateur authentifié ???? */}
+			{/* OUI */}
+
+			{/* NON */}
+
 			<Stack.Screen
 				name='(tabs)'
 				options={{ headerShown: false }}
