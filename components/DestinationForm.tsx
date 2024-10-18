85ecd83722ae711196500512472abdb39b5cc98b
Sautiere Quentin <quentin.sautiere@etudiant.univ-lr.fr>
2024-10-18 16:18:07 +0200

    add switch button, improve input styles

--- components/DestinationForm.tsx
+++ components/DestinationForm.tsx
@@ -1,5 +1,4 @@
 import {
-	Button,
 	StyleProp,
 	StyleSheet,
 	TextInput,
@@ -9,6 +8,7 @@ import {
 import React, { SetStateAction } from "react";
 import CustomButton from "./CustomButton";
 import { destinationSearch } from "../types.ts";
+import Octicons from "@expo/vector-icons/Octicons";
 
 type props = {
 	style?: StyleProp<ViewStyle>;
@@ -22,15 +22,20 @@ export default function DestinationForm({
 	submitForm,
 }: props) {
 	return (
-		<View style={styles.container}>
-			<TextInput
-				placeholder='Entrez votre position'
-				style={styles.searchInput}
-			/>
-			<TextInput
-				placeholder='Entrez votre destination'
-				style={styles.searchInput}
-			/>
+		<View style={[styles.container, style]}>
+			<View style={styles.inputsContainer}>
+				<View style={styles.searchInputOnly}>
+					<TextInput
+						placeholder='Entrez votre position'
+						style={styles.searchInput}
+					/>
+					<TextInput
+						placeholder='Entrez votre destination'
+						style={styles.searchInput}
+					/>
+				</View>
+				<Octicons name="arrow-switch" size={24} color="black" style={styles.switchButton}/>
+			</View>
 			<View style={styles.submitButtonContainer}>
 				<CustomButton
 						title='Rechercher'
@@ -51,16 +56,41 @@ const styles = StyleSheet.create({
 		gap: 10,
 		width: "75%",
 	},
+	switchButton: {
+		borderWidth: 2,
+		height: 40,
+		width: 40,
+		paddingVertical: 8,
+		paddingHorizontal: 10,
+		margin: "auto",
+		borderRadius: 5,
+		transform: [{ rotate: '90deg' }],
+	},
+	inputsContainer: {
+		display: "flex",
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: "space-between",
+		width: "100%",
+		gap: 30
+	},
 	searchInput: {
 		borderWidth: 1,
 		paddingVertical: 10,
 		paddingHorizontal: 15,
 		width: "100%",
 		borderRadius: 10,
+		flex: 1,
 	},
 	// style du conteneur du parent pour son integration dans la page
 	submitButtonContainer: {
 		marginTop: 5,
+	},
+	searchInputOnly: {
+		display: "flex",
+		height: 93,
+		width: "85%",
+		gap: 10,
 	},
 	// style pour personaliser le boutton
 	submitButton: {
