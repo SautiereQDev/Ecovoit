beb6bc1973e3f60444ae7a1ee4656ea032520b63
Sautiere Quentin <quentin.sautiere@etudiant.univ-lr.fr>
2024-10-18 16:20:36 +0200

    quick fixs

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import {StyleSheet, View} from "react-native";
+import {StyleSheet} from "react-native";
 import {ThemedText} from "@/components/ThemedText";
 import DestinationForm from "@/components/DestinationForm";
 import {useState} from "react";
--- app/(tabs)/tests.tsx
+++ app/(tabs)/tests.tsx
@@ -2,7 +2,6 @@
  * Cette page va nous servir en phase de développement pour tester des composants
  */
 
-import { View } from "react-native";
 import {SafeAreaView} from "react-native-safe-area-context";
 
 export default function Tests() {
