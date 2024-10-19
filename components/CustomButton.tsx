5838cfedce3f913029b89cf707ae2eec5499381f
Sautiere Quentin <quentin.sautiere@etudian.univ-lr.fr>
2024-10-19 17:56:00 +0200

    switch TouchableOpacity to Pressable on CustomButton component

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,54 +1,53 @@
-import {
-	ButtonProps,
-	StyleProp,
-	StyleSheet,
-	TouchableOpacity,
-	ViewStyle,
-} from "react-native";
+import {Pressable, PressableProps, StyleProp, StyleSheet, ViewStyle,} from "react-native";
 import React from "react";
-import { ThemedText } from "@/components/ThemedText.tsx";
-import { Colors } from "@/constants/Colors";
+import {ThemedText} from "@/components/ThemedText.tsx";
+import {Colors} from "@/constants/Colors";
 
-type Props = ButtonProps & {
+type Props = PressableProps & {
 	buttonStyle?: StyleProp<ViewStyle>;
+	text: string;
 	textProps?: {
 		type?:
-			| "header1"
-			| "header2"
-			| "header3"
-			| "header4"
-			| "header5"
-			| "small"
-			| "italic"
-			| "accent"
-			| "defaultBody";
+				| "header1"
+				| "header2"
+				| "header3"
+				| "header4"
+				| "header5"
+				| "small"
+				| "italic"
+				| "accent"
+				| "defaultBody";
 		color: keyof typeof Colors.light;
 	};
 };
 
 export default function CustomButton({
-	buttonStyle,
-	title,
-	textProps,
-	...restProps
+buttonStyle,
+text,
+textProps,
+...restProps
 }: Props) {
 	return (
-		<TouchableOpacity
-			style={buttonStyle}
-			{...restProps}
-		>
-			<ThemedText
-				type={textProps?.type ?? "accent"}
-				color={textProps?.color}
-				style={styles.text} // Separate style for the text
-			>
-				{title}
-			</ThemedText>
-		</TouchableOpacity>
+			<Pressable style={[styles.button, buttonStyle]} {...restProps}>
+				<ThemedText
+						type={textProps?.type ?? "defaultBody"}
+						color={textProps?.color}
+						style={styles.text}
+				>
+					{text}
+				</ThemedText>
+			</Pressable>
 	);
 }
 
 const styles = StyleSheet.create({
+	button: {
+		alignItems: "center",
+		justifyContent: "center",
+		paddingVertical: 10,
+		paddingHorizontal: 15,
+		borderRadius: 10,
+	},
 	text: {
 		textAlign: "center",
 	},
