29f11e57668734020e06bbad28f696d6edc386e7
Sautiere Quentin <quentin.sautiere@etudiant.univ-lr.fr>
2024-10-23 11:00:04 +0200

    add date props on TripCard

--- components/TripCard.tsx
+++ components/TripCard.tsx
@@ -1,14 +1,22 @@
 import { StyleSheet, Text, View, ViewStyle } from "react-native";
 import React from "react";
 import TripLabel from "@/components/UI/TripLabel.tsx";
+import { Colors } from "@/constants/Colors.ts";
+
+const backgroundColor = {
+	"en cours": Colors.light.accent,
+	effectué: Colors.light.primary,
+	annulé: Colors.light.primary,
+};
 
 type Props = {
-	style: ViewStyle;
+	style?: ViewStyle;
 	data: {
 		depart: string;
 		destination: string;
 		status: "en cours" | "terminé" | "annulé";
 		nom: string;
+		date: string;
 	};
 };
 
@@ -22,7 +30,7 @@ export default function TripCard({ style, data }: Readonly<Props>) {
 }
 const styles = StyleSheet.create({
 	container: {
-		width: 100,
+		width: "100%",
 		overflow: "hidden",
 	},
 });
