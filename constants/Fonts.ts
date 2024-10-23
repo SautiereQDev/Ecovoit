7fdc022f2e093405339a19fbb84b24b4146f055c
Sautiere Quentin <quentin.sautiere@etudiant.univ-lr.fr>
2024-10-23 22:38:11 +0200

    fix type bugs

--- constants/Fonts.ts
+++ constants/Fonts.ts
@@ -1,44 +1,44 @@
-export const fonts: {
+export const fonts = {
 	defaultBody: {
-		fontFamily: "Inter";
-		fontSize: 16;
-	};
+		fontFamily: "Inter",
+		fontSize: 16,
+	},
 	italic: {
-		fontFamily: "Inter-italic";
-		fontStyle: "italic";
-		fontSize: 16;
-	};
+		fontFamily: "Inter-italic",
+		fontStyle: "italic" as "italic",
+		fontSize: 16,
+	},
 	accent: {
-		fontFamily: "Inter";
-		fontWeight: "bold";
-		fontSize: 19;
-		letterSpacing: 0.25;
-	};
+		fontFamily: "Inter",
+		fontWeight: "bold" as "bold",
+		fontSize: 19,
+		letterSpacing: 0.25,
+	},
 	small: {
-		fontFamily: "Inter";
-		fontSize: 12;
-	};
+		fontFamily: "Inter",
+		fontSize: 12,
+	},
 	header1: {
-		fontFamily: "Gabarito-bold";
-		fontSize: 60.5;
-	};
+		fontFamily: "Gabarito-bold",
+		fontSize: 60.5,
+	},
 	header2: {
-		fontFamily: "Gabarito-bold";
-		fontSize: 46;
-	};
+		fontFamily: "Gabarito-bold",
+		fontSize: 46,
+	},
 	header3: {
-		fontFamily: "Gabarito-bold";
-		fontSize: 35.5;
-		lineHeight: 45;
-	};
+		fontFamily: "Gabarito-bold",
+		fontSize: 35.5,
+		lineHeight: 45,
+	},
 	header4: {
-		fontFamily: "Gabarito-bold";
-		fontSize: 28.5;
-	};
+		fontFamily: "Gabarito-bold",
+		fontSize: 28.5,
+	},
 	header5: {
-		fontFamily: "Gabarito-medium";
-		fontSize: 21;
-	};
+		fontFamily: "Gabarito-medium",
+		fontSize: 21,
+	},
 };
 
 export default fonts;
