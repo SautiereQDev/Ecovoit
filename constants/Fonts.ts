f262762152fdf73596b8ecae23c848863f56edf8
Sautiere Quentin <quentin.sautiere@etudiant.univ-lr.fr>
2024-10-23 21:19:29 +0200

    add fonts constants

---
+++ constants/Fonts.ts
@@ -0,0 +1,44 @@
+export const fonts: {
+	defaultBody: {
+		fontFamily: "Inter";
+		fontSize: 16;
+	};
+	italic: {
+		fontFamily: "Inter-italic";
+		fontStyle: "italic";
+		fontSize: 16;
+	};
+	accent: {
+		fontFamily: "Inter";
+		fontWeight: "bold";
+		fontSize: 19;
+		letterSpacing: 0.25;
+	};
+	small: {
+		fontFamily: "Inter";
+		fontSize: 12;
+	};
+	header1: {
+		fontFamily: "Gabarito-bold";
+		fontSize: 60.5;
+	};
+	header2: {
+		fontFamily: "Gabarito-bold";
+		fontSize: 46;
+	};
+	header3: {
+		fontFamily: "Gabarito-bold";
+		fontSize: 35.5;
+		lineHeight: 45;
+	};
+	header4: {
+		fontFamily: "Gabarito-bold";
+		fontSize: 28.5;
+	};
+	header5: {
+		fontFamily: "Gabarito-medium";
+		fontSize: 21;
+	};
+};
+
+export default fonts;
