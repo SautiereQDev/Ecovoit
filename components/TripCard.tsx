e4380f277ac49dc44543aa7a34572216e28c2539
Sautiere Quentin <quentin.sautiere@etudiant.univ-lr.fr>
2024-10-23 11:33:32 +0200

    put all element on Card

--- components/TripCard.tsx
+++ components/TripCard.tsx
@@ -1,7 +1,8 @@
-import { StyleSheet, Text, View, ViewStyle } from "react-native";
+import { Image, StyleSheet, View, ViewStyle } from "react-native";
 import React from "react";
 import TripLabel from "@/components/UI/TripLabel.tsx";
 import { Colors } from "@/constants/Colors.ts";
+import { ThemedText } from "@/components/ThemedText.tsx";
 
 const backgroundColor = {
 	"en cours": Colors.light.accent,
@@ -14,7 +15,7 @@ type Props = {
 	data: {
 		depart: string;
 		destination: string;
-		status: "en cours" | "terminé" | "annulé";
+		status: "en cours" | "effectué" | "annulé";
 		nom: string;
 		date: string;
 	};
@@ -22,15 +23,36 @@ type Props = {
 
 export default function TripCard({ style, data }: Readonly<Props>) {
 	return (
-		<View style={[styles.container, style]}>
-			<Text>TripCard</Text>
-			<TripLabel status={"en cours"} />
+		<View
+			style={[
+				styles.container,
+				style,
+				{ backgroundColor: backgroundColor[data.status] },
+			]}
+		>
+			<Image
+				source={require("@/assets/images/user-picture.jpg")}
+				style={styles.userImage}
+			/>
+			<ThemedText color={"background"}>{data.nom}</ThemedText>
+			<TripLabel status={data.status} />
+			<ThemedText color='background'>
+				{data.depart}
+				{"->"}
+				{data.destination}
+			</ThemedText>
+			<ThemedText color='background'>{data.date}</ThemedText>
 		</View>
 	);
 }
 const styles = StyleSheet.create({
 	container: {
-		width: "100%",
+		// width: "100%",
 		overflow: "hidden",
+	},
+	userImage: {
+		width: 50,
+		height: 50,
+		borderRadius: 9999,
 	},
 });
