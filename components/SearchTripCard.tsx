6704719506ca6f13fddac29b2a17609def1c1a19
Sautiere Quentin <quentin.sautiere@etudiant.univ-lr.fr>
2024-10-30 12:46:41 +0100

    merge this fucking branch

--- components/SearchTripCard.tsx
+++ components/SearchTripCard.tsx
@@ -1,7 +1,7 @@
 import { Image, StyleSheet, View, ViewStyle } from "react-native";
 import React from "react";
 import { Colors } from "@/constants/Colors.ts";
-import { ThemedText } from "@/components/ThemedText.tsx";
+import { ThemedText } from "@/components/ThemedText";
 import { SearchTripCardType } from "@/types.ts";
 
 type Props = {
