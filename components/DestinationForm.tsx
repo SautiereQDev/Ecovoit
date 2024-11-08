ad7f42ebe3524aaef941e4c2ad36ff67c08e930a
Sautiere Quentin <quentin.sautiere@etudiant.univ-lr.fr>
2024-11-08 14:28:55 +0100

    Revert "create TextInput component"
    This reverts commit faa9ebb86f0ca5dea08a391290f0b582a3be33ac.

--- components/DestinationForm.tsx
+++ components/DestinationForm.tsx
@@ -1,10 +1,15 @@
-import { StyleProp, StyleSheet, View, ViewStyle } from "react-native";
+import {
+	StyleProp,
+	StyleSheet,
+	TextInput,
+	View,
+	ViewStyle,
+} from "react-native";
 import React, { SetStateAction } from "react";
 import CustomButton from "./UI/CustomButton";
 import { destinationSearch } from "../types.ts";
 import { Colors } from "@/constants/Colors.ts";
 import IconButton from "@/components/UI/IconButton.tsx";
-import TextInput from "@/components/TextInput.tsx";
 
 type props = {
 	style?: StyleProp<ViewStyle>;
@@ -23,9 +28,12 @@ export default function DestinationForm({
 				<View style={styles.searchForm}>
 					<TextInput
 						placeholder='Entrez votre position'
-						cursorColor={Colors.light.inputText}
+						style={styles.searchInput}
 					/>
-					<TextInput placeholder='Entrez votre destination' />
+					<TextInput
+						placeholder='Entrez votre destination'
+						style={styles.searchInput}
+					/>
 					<CustomButton
 						text='Rechercher'
 						buttonStyle={styles.submitButton}
@@ -67,6 +75,14 @@ const styles = StyleSheet.create({
 		flexDirection: "row",
 		width: "100%",
 		gap: 30,
+	},
+	searchInput: {
+		borderWidth: 1,
+		paddingVertical: 10,
+		paddingHorizontal: 15,
+		width: "100%",
+		borderRadius: 10,
+		flex: 1,
 	},
 	// style du conteneur du parent pour son integration dans la page
 	searchForm: {
