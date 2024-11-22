786a05b1aeeebf8daa12f10714c53148f1de59c8
Maxime Chasles <mchasles@MacBook-Air-de-Maxime.local>
2024-11-22 15:16:24 +0100

    config/ tunnel pretierrc

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,15 @@
-import { Slot } from "expo-router";
-import { SessionProvider } from "@/components/context/SessionProvider";
-import { MockServiceProvider } from "@/components/context/MockServiceProvider";
-import { LocationProvider } from "@/components/context/LocationProvider";
+import { Slot } from 'expo-router';
+import { SessionProvider } from '@/components/context/SessionProvider';
+import { MockServiceProvider } from '@/components/context/MockServiceProvider';
+import { LocationProvider } from '@/components/context/LocationProvider';
 
 export default function RootLayout() {
-  // console.warn("-- render RootLayout");
-  return (
-    <SessionProvider>
-      <LocationProvider>
-        <Slot />
-      </LocationProvider>
-    </SessionProvider>
-  );
+	// console.warn("-- render RootLayout");
+	return (
+		<SessionProvider>
+			<LocationProvider>
+				<Slot />
+			</LocationProvider>
+		</SessionProvider>
+	);
 }
