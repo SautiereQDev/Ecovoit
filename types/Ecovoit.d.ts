d178bab8abc459dad8c920c6ee8abf9556875ea2
Maxime Chasles <mchasles@etudiant.univ-lr.fr>
2024-11-28 19:35:48 +0100

    other/ update ecovoit types

--- types/Ecovoit.d.ts
+++ types/Ecovoit.d.ts
@@ -1,34 +1,65 @@
 export type User = {
 	id: number;
-	firstName: string | null;
+	firstName: string;
 	lastName: string | null;
-	username: string | null;
-	email: string | null;
+	username: string;
+	email: string;
 	bio: string | null;
-	rank: "member" | "moderator" | "admin";
+	rank: 'member' | 'moderator' | 'admin';
 	verified: boolean;
 	vehicles: Vehicle[];
-	tripsAsDriver: Trip[];
-	tripsAsPassenger: Trip[];
+	tripsAsDriver: string[];
+	tripsAsPassenger: string[];
 };
 
 export type Vehicle = {
 	owner: number;
-	label: string | null;
+	label: string;
 	consumption: number | null;
 	emission: number | null;
 };
 
+export type Location = {
+	name: string;
+	longitude: string;
+	latitude: number;
+};
+
 export type Point = {
-	type: "start" | "end" | "checkpoint";
+	id: string;
+	trip: string;
+	type: 'start' | 'end' | 'checkpoint';
 	locationName: string;
+	location: location;
+	waitingTime: number;
 	previous: number | null;
 	next: number | null;
 };
 
 export type Trip = {
-	vehicle: string | null;
-	seats: number | null;
-	datetime: number | null;
-	points: Point[];
+	id: string;
+	driver: string;
+	vehicle: string;
+	seats: number;
+	datetime: number;
+	description: string | null;
+	points: point[];
+	distance: number | null;
+	duration: number | null;
+	availableSeats: number;
+};
+
+export type Review = {
+	emitter: string;
+	receiver: string;
+	day: number;
+	stars: 0 | 1 | 2 | 3 | 4 | 5;
+	title: string | null;
+	content: string | null;
+	deletionRequested: boolean;
+};
+
+export type Passenger = {
+	passenger: string;
+	trip: string;
 };
