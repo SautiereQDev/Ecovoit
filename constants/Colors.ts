41be80fc356329653359e1bdb2d735e7a3e37f6e
Maxime Chasles <mchasles@etudiant.univ-lr.fr>
2024-11-29 03:11:57 +0100

    design/ add a temporary dark theme

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,29 +3,29 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const resetButton = "#d60d0d";
+const resetButton = '#d60d0d';
 
 export const Colors = {
 	light: {
-		hidden: "#DCDCDC",
-		text: "#11181C",
-		primary: "#00570D",
-		secondary: "#106EB2",
-		accent: "#5bb6c6",
-		background: "#FDFFFD",
-		inputText: "#616161",
-		inputBackground: "#E6E6E6",
+		hidden: '#DCDCDC',
+		text: '#11181C',
+		primary: '#00570D',
+		secondary: '#106EB2',
+		accent: '#5bb6c6',
+		background: '#FDFFFD',
+		inputText: '#616161',
+		inputBackground: '#E6E6E6',
 		resetButton,
 	},
 	dark: {
-		hidden: "#DCDCDC",
-		text: "#FFF",
-		primary: "#2980b9",
-		secondary: "#27ae60",
-		background: "#000",
-		inputText: "#ccc",
-		inputBackground: "#333",
-		accent: "#c0392b",
+		hidden: '#DCDCDC',
+		text: '#FDFFFD',
+		primary: '#00570D',
+		secondary: '#106EB2',
+		accent: '#5bb6c6',
+		background: '#11181C',
+		inputText: '#616161',
+		inputBackground: '#E6E6E6',
 		resetButton,
 	},
 };
