295292a25418fdeee1e31f26c90875ed5f200b81
Quentin_Sautière <quentin.sautiere@etudiant.univ-lr.fr>
2024-11-30 19:53:02 +0100

    improve readability

--- components/pages/RegisterPage.tsx
+++ components/pages/RegisterPage.tsx
@@ -34,217 +34,214 @@ const RegisterPage = () => {
 	const handleSubmit = () => {
 		alert('Inscription réussie');
 		router.navigate('/');
-		// axios.post
 	};
 
-	function nextStep() {
-		setStep(step + 1);
-	}
+	const nextStep = () => setStep(step + 1);
+	const previousStep = () => setStep(step - 1);
+	const goHome = () => router.navigate('/');
 
-	function previousStep() {
-		setStep(step - 1);
-	}
+	const renderStep1 = () => (
+		<SafeAreaView style={styles.container}>
+			<View style={styles.content}>
+				<Link
+					style={[styles.buttonHome, styles.buttonPrevius]}
+					href={'/'}
+				>
+					<ThemedText
+						type={'accent'}
+						style={styles.buttonHomeText}
+					>
+						Retour
+					</ThemedText>
+				</Link>
+				<ThemedText
+					type={'header2'}
+					style={styles.title}
+				>
+					Inscription
+				</ThemedText>
+				<ThemedInput
+					placeholder='Pseudo'
+					onChangeText={(value) =>
+						setFormData({ ...formData, username: value })
+					}
+				/>
+				<ThemedInput
+					placeholder='Email'
+					onChangeText={(value) => setFormData({ ...formData, email: value })}
+					keyboardType={'email-address'}
+				/>
+				<ThemedInput
+					placeholder='Mot de passe'
+					onChangeText={(value) =>
+						setFormData({ ...formData, password: value })
+					}
+					secureTextEntry={true}
+				/>
+				<CustomButton
+					text='Suivant'
+					textProps={{ color: 'background' }}
+					backgroundColor={'primary'}
+					onPress={nextStep}
+					buttonStyle={styles.buttonNext}
+				/>
+			</View>
+		</SafeAreaView>
+	);
 
-	function goHome() {
-		router.navigate('/');
-	}
+	const renderStep2 = () => (
+		<SafeAreaView style={styles.container}>
+			<View style={styles.content}>
+				<CustomButton
+					text={'Retour'}
+					textProps={{ color: 'background' }}
+					backgroundColor={'secondary'}
+					buttonStyle={styles.buttonPrevius}
+					onPress={previousStep}
+				/>
+				<View style={styles.header}>
+					<ThemedText
+						type={'header3'}
+						style={styles.title}
+					>
+						Informations personelles
+					</ThemedText>
+					<ThemedText
+						type={'header6'}
+						style={styles.paragraph}
+					>
+						Afin de vous identifiez en tant que conducteur veuillez entrer les
+						informations de votre véhicule
+					</ThemedText>
+				</View>
+				<View style={styles.formulaire}>
+					<ThemedInput
+						placeholder='Prénom'
+						onChangeText={(value) =>
+							setFormData({ ...formData, firstName: value })
+						}
+					/>
+					<ThemedInput
+						placeholder='Nom'
+						onChangeText={(value) =>
+							setFormData({ ...formData, lastName: value })
+						}
+					/>
+					<CustomButton
+						text="Finaliser l'inscription"
+						textProps={{ color: 'background' }}
+						backgroundColor={'primary'}
+						onPress={nextStep}
+						buttonStyle={styles.buttonNext}
+					/>
+				</View>
+			</View>
+		</SafeAreaView>
+	);
+
+	const renderStep3 = () => (
+		<SafeAreaView style={styles.container}>
+			<View style={styles.confirmationPage}>
+				<CustomButton
+					text={'Retour'}
+					textProps={{ color: 'background' }}
+					backgroundColor={'secondary'}
+					buttonStyle={styles.buttonPrevius}
+					onPress={previousStep}
+				/>
+				<ThemedText
+					type={'header3'}
+					style={styles.confirmationPage}
+				>
+					Souhaitez vous enregistrer votre véhicule ?
+				</ThemedText>
+				<View style={styles.buttons}>
+					<CustomButton
+						text={'Oui'}
+						backgroundColor={'acceptButton'}
+						textProps={{ color: 'background' }}
+						buttonStyle={styles.askButton}
+						onPress={nextStep}
+					/>
+					<CustomButton
+						text={'Non'}
+						backgroundColor={'deniedButton'}
+						textProps={{ color: 'background' }}
+						buttonStyle={styles.askButton}
+						onPress={goHome}
+					/>
+				</View>
+			</View>
+		</SafeAreaView>
+	);
+
+	const renderStep4 = () => (
+		<SafeAreaView style={styles.container}>
+			<View style={styles.confirmationPage}>
+				<CustomButton
+					text={'Retour'}
+					textProps={{ color: 'background' }}
+					backgroundColor={'secondary'}
+					buttonStyle={styles.buttonPrevius}
+					onPress={previousStep}
+				/>
+				<View style={styles.header}>
+					<ThemedText
+						type={'header3'}
+						style={styles.title}
+					>
+						Enregistrement du véhicule
+					</ThemedText>
+					<ThemedText
+						type={'header6'}
+						style={styles.paragraph}
+					>
+						Afin de vous identifiez en tant que conducteur veuillez entrer les
+						informations de votre véhicule
+					</ThemedText>
+				</View>
+				<View style={styles.formulaire}>
+					<ThemedInput
+						placeholder='Nom du véhicule'
+						onChangeText={(value) =>
+							setFormData({ ...formData, carName: value })
+						}
+					/>
+					<ThemedInput
+						placeholder='Consommation moyenne'
+						onChangeText={(value) =>
+							setFormData({ ...formData, carConsommation: Number(value) })
+						}
+						keyboardType={'numeric'}
+					/>
+					<ThemedInput
+						placeholder='Emission de CO2'
+						onChangeText={(value) =>
+							setFormData({ ...formData, carEmission: Number(value) })
+						}
+						keyboardType={'numeric'}
+					/>
+					<CustomButton
+						text='Enregistrer'
+						textProps={{ color: 'background' }}
+						backgroundColor={'primary'}
+						onPress={handleSubmit}
+						buttonStyle={styles.buttonNext}
+					/>
+				</View>
+			</View>
+		</SafeAreaView>
+	);
 
 	switch (step) {
 		case 1:
-			return (
-				<SafeAreaView style={styles.container}>
-					<View style={styles.content}>
-						<Link
-							style={[styles.buttonHome, styles.buttonPrevius]}
-							href={'/'}
-						>
-							<ThemedText
-								type={'accent'}
-								style={styles.buttonHomeText}
-							>
-								Retour
-							</ThemedText>
-						</Link>
-						<ThemedText
-							type={'header2'}
-							style={styles.title}
-						>
-							Inscription
-						</ThemedText>
-						<ThemedInput
-							placeholder='Pseudo'
-							onChangeText={(value) =>
-								setFormData({ ...formData, username: value })
-							}
-						/>
-						<ThemedInput
-							placeholder='Email'
-							onChangeText={(value) =>
-								setFormData({ ...formData, email: value })
-							}
-							keyboardType={'email-address'}
-						/>
-						<ThemedInput
-							placeholder='Mot de passe'
-							onChangeText={(value) =>
-								setFormData({ ...formData, password: value })
-							}
-							secureTextEntry={true}
-						/>
-						<CustomButton
-							text='Suivant'
-							textProps={{ color: 'background' }}
-							backgroundColor={'primary'}
-							onPress={nextStep}
-							buttonStyle={styles.buttonNext}
-						/>
-					</View>
-				</SafeAreaView>
-			);
+			return renderStep1();
 		case 2:
-			return (
-				<SafeAreaView style={styles.container}>
-					<View style={styles.content}>
-						<CustomButton
-							text={'Retour'}
-							textProps={{ color: 'background' }}
-							backgroundColor={'secondary'}
-							buttonStyle={styles.buttonPrevius}
-							onPress={previousStep}
-						/>
-						<View style={styles.header}>
-							<ThemedText
-								type={'header3'}
-								style={styles.title}
-							>
-								Informations personelles
-							</ThemedText>
-							<ThemedText
-								type={'header6'}
-								style={styles.paragraph}
-							>
-								Afin de vous identifiez en tant que conducteur veuillez entrer
-								les informations de votre véhicule
-							</ThemedText>
-						</View>
-						<View style={styles.formulaire}>
-							<ThemedInput
-								placeholder='Prénom'
-								onChangeText={(value) =>
-									setFormData({ ...formData, firstName: value })
-								}
-							/>
-							<ThemedInput
-								placeholder='Nom'
-								onChangeText={(value) =>
-									setFormData({ ...formData, lastName: value })
-								}
-							/>
-							<CustomButton
-								text="Finaliser l'inscription"
-								textProps={{ color: 'background' }}
-								backgroundColor={'primary'}
-								onPress={nextStep}
-								buttonStyle={styles.buttonNext}
-							/>
-						</View>
-					</View>
-				</SafeAreaView>
-			);
+			return renderStep2();
 		case 3:
-			return (
-				<SafeAreaView style={styles.container}>
-					<View style={styles.confirmationPage}>
-						<CustomButton
-							text={'Retour'}
-							textProps={{ color: 'background' }}
-							backgroundColor={'secondary'}
-							buttonStyle={styles.buttonPrevius}
-							onPress={previousStep}
-						/>
-						<ThemedText
-							type={'header3'}
-							style={styles.confirmationPage}
-						>
-							Souhaitez vous enregistrer votre véhicule ?
-						</ThemedText>
-						<View style={styles.buttons}>
-							<CustomButton
-								text={'Oui'}
-								backgroundColor={'acceptButton'}
-								textProps={{ color: 'background' }}
-								buttonStyle={styles.askButton}
-								onPress={nextStep}
-							/>
-							<CustomButton
-								text={'Non'}
-								backgroundColor={'deniedButton'}
-								textProps={{ color: 'background' }}
-								buttonStyle={styles.askButton}
-								onPress={goHome}
-							/>
-						</View>
-					</View>
-				</SafeAreaView>
-			);
+			return renderStep3();
 		case 4:
-			return (
-				<SafeAreaView style={styles.container}>
-					<View style={styles.confirmationPage}>
-						<CustomButton
-							text={'Retour'}
-							textProps={{ color: 'background' }}
-							backgroundColor={'secondary'}
-							buttonStyle={styles.buttonPrevius}
-							onPress={previousStep}
-						/>
-						<View style={styles.header}>
-							<ThemedText
-								type={'header3'}
-								style={styles.title}
-							>
-								Enregistrement du véhicule
-							</ThemedText>
-							<ThemedText
-								type={'header6'}
-								style={styles.paragraph}
-							>
-								Afin de vous identifiez en tant que conducteur veuillez entrer
-								les informations de votre véhicule
-							</ThemedText>
-						</View>
-						<View style={styles.formulaire}>
-							<ThemedInput
-								placeholder='Nom du véhicule'
-								onChangeText={(value) =>
-									setFormData({ ...formData, carName: value })
-								}
-							/>
-							<ThemedInput
-								placeholder='Consommation moyenne'
-								onChangeText={(value) =>
-									setFormData({ ...formData, carConsommation: Number(value) })
-								}
-								keyboardType={'numeric'}
-							/>
-							<ThemedInput
-								placeholder='Emission de CO2'
-								onChangeText={(value) =>
-									setFormData({ ...formData, carEmission: Number(value) })
-								}
-								keyboardType={'numeric'}
-							/>
-							<CustomButton
-								text='Enregistrer'
-								textProps={{ color: 'background' }}
-								backgroundColor={'primary'}
-								onPress={handleSubmit}
-								buttonStyle={styles.buttonNext}
-							/>
-						</View>
-					</View>
-				</SafeAreaView>
-			);
+			return renderStep4();
 		default:
 			throw new Error('Invalid step');
 	}
@@ -300,7 +297,6 @@ const styles = StyleSheet.create({
 		borderRadius: 10,
 		backgroundColor: Colors.light.secondary,
 	},
-
 	buttonHomeText: {
 		color: Colors.light.background,
 		textAlign: 'center',
