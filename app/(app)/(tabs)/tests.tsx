27bb93c9af17746c549765935c9ad04e6d316788
Quentin_Sautière <quentin.sautiere@etudian.univ-lr.fr>
2024-12-10 22:19:50 +0100

    add test file
    Signed-off-by: Quentin_Sautière <quentin.sautiere@etudiant.univ-lr.fr>

--- app/(app)/(tabs)/tests.tsx
+++ app/(app)/(tabs)/tests.tsx
@@ -1,5 +1,13 @@
-import RegisterPage from '@/components/pages/RegisterPage';
+import { CustomButton } from '@/components';
+import { router } from 'expo-router';
 
 export default function Tests() {
-	return <RegisterPage />;
+	return (
+		<CustomButton
+			text={'Inscritpion'}
+			onPress={() => {
+				router.push('/register');
+			}}
+		/>
+	);
 }
