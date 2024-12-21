2b0972946926fe29e382730598123e91db697264
Quentin_Sautière <quentin.sautiere@etudiant.univ-lr.fr>
2024-12-21 16:49:05 +0100

    add pre-build size on customButton
    Signed-off-by: Quentin_Sautière <quentin.sautiere@etudiant.univ-lr.fr>

--- components/buttons/CustomButton.tsx
+++ components/buttons/CustomButton.tsx
@@ -4,63 +4,67 @@ import {
 	StyleProp,
 	StyleSheet,
 	ViewStyle,
-} from "react-native";
-import React from "react";
-import { ThemedText } from "@/components/texts/ThemedText";
-import { Colors } from "@/constants/Colors";
+} from 'react-native';
+import React, { ReactNode } from 'react';
+import { ThemedText } from '@/components/texts/ThemedText';
+import { Colors } from '@/constants/Colors';
 
 type Props = PressableProps & {
 	buttonStyle?: StyleProp<ViewStyle>;
 	text: string;
 	textProps?: {
 		type?:
-			| "header1"
-			| "header2"
-			| "header3"
-			| "header4"
-			| "header5"
-			| "header6"
-			| "small"
-			| "italic"
-			| "accent"
-			| "defaultBody"
-			| "bigger"
+			| 'header1'
+			| 'header2'
+			| 'header3'
+			| 'header4'
+			| 'header5'
+			| 'header6'
+			| 'small'
+			| 'italic'
+			| 'accent'
+			| 'defaultBody'
+			| 'bigger';
 		color?: keyof typeof Colors.light;
 	};
 	backgroundColor?: keyof typeof Colors.light;
+	size?: 'default' | 'bigger' | 'smaller';
 };
 
 /**
- * CustomButton component renders a pressable button with customizable text.
+ * Composant CustomButton qui rend un bouton pressable avec un texte personnalisable.
  *
- * @param {StyleProp<ViewStyle>} buttonStyle - Optional style for the button.
- * @param {string} text - The text to display inside the button.
- * @param {Object} textProps - Optional properties for the text.
- * @param {string} textProps.type - The type of text style.
- * @param {keyof typeof Colors.light} textProps.color - The color of the text.
- * @param {keyof typeof Colors.light} backgroundColor - The background color of the button.
- * @param {PressableProps} restProps - Additional props for the Pressable component.
+ * @param {StyleProp<ViewStyle>} buttonStyle - Style optionnel pour le bouton.
+ * @param {string} text - Le texte à afficher à l'intérieur du bouton.
+ * @param {Object} textProps - Propriétés optionnelles pour le texte.
+ * @param {string} textProps.type - Le type de style de texte.
+ * @param {keyof typeof Colors.light} textProps.color - La couleur du texte.
+ * @param {keyof typeof Colors.light} backgroundColor - La couleur de fond du bouton.
+ * @param {'default' | 'bigger' | 'smaller'} [size] - La taille du bouton.
+ * @param {PressableProps} restProps - Props supplémentaires pour le composant Pressable.
  *
- * @returns {JSX.Element} The rendered button component.
+ * @returns {JSX.Element} Le composant bouton rendu.
  */
 export function CustomButton({
-															 buttonStyle,
-															 text,
-															 textProps = { color: "background" },
-															 backgroundColor = "primary",
-															 ...restProps
-														 }: Props): JSX.Element {
+	buttonStyle,
+	text,
+	textProps = { color: 'background' },
+	backgroundColor = 'primary',
+	size = 'default',
+	...restProps
+}: Props): ReactNode {
 	return (
 		<Pressable
 			style={[
 				styles.button,
+				styles[size], // Appliquer les styles spécifiques à la taille
 				buttonStyle,
 				{ backgroundColor: Colors.light[backgroundColor] },
 			]}
 			{...restProps}
 		>
 			<ThemedText
-				type={textProps?.type ?? "bigger"}
+				type={textProps?.type ?? 'bigger'}
 				color={textProps?.color}
 				style={styles.text}
 			>
@@ -72,13 +76,27 @@ export function CustomButton({
 
 const styles = StyleSheet.create({
 	button: {
-		alignItems: "center",
-		justifyContent: "center",
-		paddingVertical: 6,
+		alignItems: 'center',
+		justifyContent: 'center',
 		borderRadius: 10,
 	},
 	text: {
-		textAlign: "center",
+		textAlign: 'center',
+	},
+	default: {
+		paddingVertical: '2%',
+		paddingHorizontal: '4%',
+		width: '100%',
+	},
+	bigger: {
+		paddingVertical: '4%',
+		paddingHorizontal: '8%',
+		width: '80%',
+	},
+	smaller: {
+		paddingVertical: '2%',
+		paddingHorizontal: '2%',
+		width: '70%',
 	},
 });
 
