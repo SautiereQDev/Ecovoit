604f6b82e4105c4c240d5be19b655ed87f440951
Quentin_Sautière <quentin.sautiere@etudiant.univ-lr.fr>
2024-12-21 16:37:46 +0100

    CircularProgress custom UI element
    Signed-off-by: Quentin_Sautière <quentin.sautiere@etudiant.univ-lr.fr>

---
+++ components/CircularProgress.tsx
@@ -0,0 +1,91 @@
+import React, { ReactNode } from 'react';
+import { StyleSheet, View } from 'react-native';
+import Svg, { Circle } from 'react-native-svg';
+import { ThemedText } from '@/components/texts';
+import Colors from '@/constants/Colors';
+
+/**
+ * Propriétés du composant CircularProgress.
+ */
+interface CircularProgressProps {
+	size: number; // Taille du cercle
+	strokeWidth: number; // Largeur du trait du cercle
+	progress: number; // Progression en pourcentage (0-100)
+	color: string; // Couleur du cercle de progression
+	backgroundColor: string; // Couleur de fond du cercle
+	textColor?: keyof typeof Colors.light; // Couleur du texte
+}
+
+/**
+ * Composant de barre de progression circulaire avec pourcentage affiché au centre.
+ * @param {CircularProgressProps} props - Les propriétés du composant.
+ * @returns {ReactNode} Le composant de barre de progression circulaire.
+ */
+export const CircularProgress: React.FC<CircularProgressProps> = ({
+	size,
+	strokeWidth,
+	progress,
+	color,
+	backgroundColor,
+	textColor,
+}: CircularProgressProps): ReactNode => {
+	const radius = (size - strokeWidth) / 2;
+	const circumference = 2 * Math.PI * radius;
+	const strokeDashoffset = circumference - (progress / 100) * circumference;
+
+	return (
+		<View style={{ width: size, height: size }}>
+			<Svg
+				width={size}
+				height={size}
+			>
+				<Circle
+					stroke={backgroundColor}
+					fill='none'
+					cx={size / 2}
+					cy={size / 2}
+					r={radius}
+					strokeWidth={strokeWidth}
+				/>
+				<Circle
+					stroke={color}
+					fill='none'
+					cx={size / 2}
+					cy={size / 2}
+					r={radius}
+					strokeWidth={strokeWidth}
+					strokeDasharray={circumference}
+					strokeDashoffset={strokeDashoffset}
+					strokeLinecap='round'
+				/>
+			</Svg>
+			<View style={styles.textContainer}>
+				<ThemedText
+					style={styles.text}
+					color={textColor}
+				>{`${progress}%`}</ThemedText>
+			</View>
+		</View>
+	);
+};
+
+export default CircularProgress;
+
+/**
+ * Styles pour le composant CircularProgress.
+ */
+const styles = StyleSheet.create({
+	textContainer: {
+		position: 'absolute',
+		top: 0,
+		left: 0,
+		right: 0,
+		bottom: 0,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	text: {
+		fontSize: 18,
+		fontWeight: 'bold',
+	},
+});
