aef17de2e7fb92081310c10f4dbdc156bc4cf8c0
Sautiere Quentin <quentin.sautiere@etudiant.univ-lr.fr>
2024-12-24 10:56:15 +0100

    competing profilePicture page

---
+++ app/(app)/(tabs)/profile/completing/profilePicture.tsx
@@ -0,0 +1,45 @@
+import { View } from 'react-native';
+import React from 'react';
+import { CustomButton, ThemedInput, ThemedText } from '@/components';
+import {
+	globalStyle,
+	handleBack,
+} from '@/app/(app)/(tabs)/profile/completing/index';
+import ReturnButton from '@/components/buttons/ReturnButton';
+import { useRegister } from '@/context/RegisterProvider';
+import { router } from 'expo-router';
+import ImagePickerButton from '@/components/buttons/ImagePickerButton';
+
+const LastName = () => {
+	const { validateField, errors, data, setData } = useRegister();
+
+	const handleSubmit = (): void => {
+		validateField('profilePicture', data.profilePicture);
+		if (!errors.lastName) {
+			// @ts-ignore
+			router.push('/profile/completing');
+		}
+	};
+
+	const setImage = (image: string) => {
+		setData({ ...data, profilePicture: image });
+	};
+
+	return (
+		<View style={globalStyle.content}>
+			<ReturnButton handleBack={handleBack} />
+			<ThemedText type={'header4'}>Ajouter une photo de profil</ThemedText>
+			<ImagePickerButton
+				image={data.profilePicture}
+				setImage={setImage}
+			/>
+			<CustomButton
+				text={'Valider'}
+				onPress={handleSubmit}
+				buttonStyle={globalStyle.buttonNext}
+				size={"smaller"}
+			/>
+		</View>
+	);
+};
+export default LastName;
