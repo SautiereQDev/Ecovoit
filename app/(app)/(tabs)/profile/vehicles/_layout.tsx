b81fc41bd28736843cabfe8571c20e5555ebea51
Sautiere Quentin <quentin.sautiere@etudiant.univ-lr.fr>
2024-12-25 20:18:44 +0100

    add _layout and create routes

---
+++ app/(app)/(tabs)/profile/vehicles/_layout.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Stack } from 'expo-router';
+import { ProfileProvider } from '@/context/ProfileProvider';
+
+export default function ProfileLayout() {
+	return (
+		<ProfileProvider>
+			<Stack
+				screenOptions={{
+					headerShown: false,
+				}}
+			>
+				<Stack.Screen name='index' />
+				<Stack.Screen name='add' />
+				<Stack.Screen name='edit' />
+			</Stack>
+		</ProfileProvider>
+	);
+}
