813d599133292d07b73b976eb0e6a43a2dac740e
Sautiere Quentin <quentin.sautiere@etudiant.univ-lr.fr>
2024-12-28 16:31:02 +0100

    update index.ts for exports

---
+++ context/index.ts
@@ -0,0 +1,7 @@
+export * from './LocationProvider';
+export * from './MockServiceProvider';
+export * from './RegisterProvider';
+export * from './ProfileProvider';
+export * from './SearchProvider';
+export * from './SessionProvider';
+export * from './TripCreationProvider';
