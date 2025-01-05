1f5ae056a88c7638992aa378d58968fbba6b1800
Quentin_Sautière <quentin.sautiere@etudiant.univ-lr.fr>
2025-01-06 00:44:28 +0100

    PostTripProvider
    Signed-off-by: Quentin_Sautière <quentin.sautiere@etudiant.univ-lr.fr>

---
+++ src/providers/PostTripProvider.tsx
@@ -0,0 +1,56 @@
+import React, {
+	createContext,
+	ReactNode,
+	useContext,
+	useMemo,
+	useState,
+} from 'react';
+import { EVAPI } from '@ecovoit-api/mock-adapter';
+
+interface PostTripContextType {
+	postTripQuery: EVAPI.TripCreation;
+	setPostTripQuery: React.Dispatch<React.SetStateAction<EVAPI.TripCreation>>;
+}
+
+const PostTripContext = createContext<PostTripContextType | undefined>(
+	undefined
+);
+
+export const PostTripProvider: React.FC<{ children: ReactNode }> = ({
+	children,
+}) => {
+	const initialState: EVAPI.TripCreation = {
+		vehicle: '',
+		seats: 0,
+		description: null,
+		datetime: 0,
+		points: [],
+	};
+
+	const [postTripQuery, setPostTripQuery] =
+		useState<EVAPI.TripCreation>(initialState);
+
+	const value: PostTripContextType = useMemo(
+		() => ({
+			postTripQuery,
+			setPostTripQuery,
+		}),
+		[postTripQuery]
+	);
+
+	return (
+		<PostTripContext.Provider value={value}>
+			{children}
+		</PostTripContext.Provider>
+	);
+};
+
+export const usePostTripContext = (): PostTripContextType => {
+	const context = useContext(PostTripContext);
+	if (!context) {
+		throw new Error(
+			'useRegisterContext must be used within a PostTripProvider'
+		);
+	}
+	return context;
+};
