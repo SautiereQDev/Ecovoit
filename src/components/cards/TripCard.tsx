380c15320d90a32cbb43827e869f669da0086357
Quentin_Sautière <quentin.sautiere@etudiant.univ-lr.fr>
2025-01-06 19:45:21 +0100

    TripCard
    Signed-off-by: Quentin_Sautière <quentin.sautiere@etudiant.univ-lr.fr>

--- src/components/cards/TripCard.tsx
+++ src/components/cards/TripCard.tsx
@@ -1,14 +1,15 @@
-import { Image, StyleSheet, View, ViewStyle } from 'react-native';
+import { StyleSheet, View, ViewStyle } from 'react-native';
 import React from 'react';
-import { TripLabel } from '../labels/TripLabel';
+import { TripLabel } from '@/components/labels';
 import { Colors } from '@/constants/Colors';
 import { ThemedText } from '@/components/texts';
 import { EVAPI } from '@ecovoit-api/mock-adapter';
+import { Ionicons } from '@expo/vector-icons';
 
 const backgroundColor = {
-	upcoming: Colors.light.primary,
-	ongoing: Colors.light.secondary,
-	completed: Colors.light.acceptButton,
+	upcoming: Colors.light.secondary,
+	ongoing: Colors.light.accent,
+	completed: Colors.light.primary,
 	cancelled: Colors.light.error,
 };
 
@@ -27,35 +28,57 @@ export function TripCard({ style, data }: Readonly<Props>) {
 				data.status === 'cancelled' && styles.cancelledCard,
 			]}
 		>
-			<Image
-				source={require('@/assets/images/user-picture.jpg')}
-				style={styles.userImage}
-			/>
-			<View style={styles.textContainer}>
-				<View style={styles.header}>
-					<ThemedText
-						type='header4'
-						color={'background'}
-						style={{ marginLeft: 20 }}
-					>
-						<ThemedText color='background'>
-							{data.points[0].location.name}
-							{' -> '}
-							{data.points[data.points.length - 1].location.name}
-						</ThemedText>
-					</ThemedText>
-					<TripLabel
-						status={data.status}
-						style={{ alignSelf: 'baseline' }}
-					/>
-				</View>
+			<View style={styles.header}>
 				<ThemedText
-					color='background'
-					style={styles.date}
-					type={'smaller'}
+					type='header6'
+					color={'background'}
 				>
-					{data.datetime.toLocaleString()}
+					{data.points[0].location.name}
+					{' -> '}
+					{data.points[data.points.length - 1].location.name}
 				</ThemedText>
+				<TripLabel status={data.status} />
+			</View>
+			<View style={styles.body}>
+				<View style={styles.row}>
+					<ThemedText
+						color={'background'}
+						type={'accent'}
+					>
+						{data.distance}km
+					</ThemedText>
+					<ThemedText
+						color={'background'}
+						style={{ alignSelf: 'flex-end' }}
+					>
+						{data.points.length}
+						{data.points.length > 1 ? ' étapes' : ' étape'}
+					</ThemedText>
+				</View>
+				<View style={styles.row}>
+					<ThemedText
+						color='background'
+						type={'header4'}
+					>
+						{data.seats}
+						<Ionicons
+							name='man-sharp'
+							size={24}
+							color='background'
+						/>
+					</ThemedText>
+					<ThemedText
+						color='background'
+						style={styles.date}
+					>
+						{new Date(data.datetime).toLocaleDateString('fr-FR', {
+							year: 'numeric',
+							month: 'long',
+							day: 'numeric',
+							hour: 'numeric',
+						})}
+					</ThemedText>
+				</View>
 			</View>
 		</View>
 	);
@@ -64,11 +87,10 @@ export function TripCard({ style, data }: Readonly<Props>) {
 const styles = StyleSheet.create({
 	container: {
 		display: 'flex',
-		flexDirection: 'row',
-		width: '100%',
-		padding: 10,
-		overflow: 'hidden',
-		borderRadius: 5,
+		flexDirection: 'column',
+		borderRadius: 10,
+		padding: 15,
+		gap: 10,
 	},
 	userImage: {
 		width: '20%',
@@ -82,14 +104,23 @@ const styles = StyleSheet.create({
 	cancelledCard: {
 		opacity: 0.75, // Apply grayscale effect using opacity
 	},
-	textContainer: {
-		marginLeft: 'auto',
-		maxWidth: '72%',
-	},
 	header: {
+		alignItems: 'center',
 		display: 'flex',
 		flexDirection: 'row',
-		gap: 45,
+		justifyContent: 'space-between',
+	},
+	body: {
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'space-between',
+		gap: 3,
+	},
+	row: {
+		display: 'flex',
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-between',
 	},
 	date: {
 		alignSelf: 'flex-end',
