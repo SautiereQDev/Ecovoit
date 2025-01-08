0c1ff5b6e01f9ac1e56816fda933f8f09b348c93
Quentin_Sautière <quentin.sautiere@etudiant.univ-lr.fr>
2025-01-08 18:16:18 +0100

    join page
    Signed-off-by: Quentin_Sautière <quentin.sautiere@etudiant.univ-lr.fr>

--- app/(app)/trips/[id]/join.tsx
+++ app/(app)/trips/[id]/join.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import { useLocalSearchParams } from 'expo-router';
+import { router, useLocalSearchParams } from 'expo-router';
 import { useData } from '@/providers';
 import { Image, ScrollView, StyleSheet, View } from 'react-native';
 import { ErrorScreen, LoadingScreen } from '@/components/pages';
-import { ReturnButton } from '@/components/buttons';
+import { CustomButton, ReturnButton } from '@/components/buttons';
 import { ThemedText } from '@/components/texts';
 import { TripInfoLabel, TripLabel } from '@/components/labels';
 import { RouteMap } from '@/components/map';
-import { Colors } from '@/constants';
 import { Stars } from '@/components/UI';
 import { EVAPI } from '@ecovoit-api/mock-adapter';
-import { globalStyles } from '@/styles';
 import { getHoursAndMinutes } from '@/utils';
+import { detailTripStyles, globalStyles } from '@/styles';
+import { notify } from 'react-native-notificated';
 
 type ratingType = 1 | 2 | 3 | 4 | 5 | 0.5 | 1.5 | 2.5 | 3.5 | 4.5;
 
 const DetailedTripPage = () => {
 	const { id } = useLocalSearchParams<{ id: string }>();
-	const { useTrip, useVehicleByUserByLabel, useUser } = useData();
+	const { useTrip, useVehicleByUserByLabel, useUser, useAddPassengerToTrip } =
+		useData();
 	const { data: trips, isLoading, error: errorTrip } = useTrip(id);
+	const addPassengerToTrip = useAddPassengerToTrip();
 
 	const {
 		data: vehicle,
@@ -44,28 +46,70 @@ const DetailedTripPage = () => {
 		)
 		.map((point) => point.location);
 
+	const handleSubscribe = () => {
+		addPassengerToTrip.mutate(
+			{ tripId: id },
+			{
+				onError: (error) => {
+					console.error(error);
+					if (error.status === 409) {
+						notify('error', {
+							params: {
+								title: 'Erreur',
+								description: 'Vous êtes déjà inscrit à ce trajet',
+							},
+						});
+						router.push('/');
+					} else {
+						notify('error', {
+							params: {
+								title: 'Erreur',
+								description: `Une erreur est survenue lors de l'opration`,
+							},
+						});
+					}
+				},
+				onSuccess: () => {
+					notify('success', {
+						params: {
+							title: 'Opération effectué',
+							description: 'Vous avez été ajouté au trajet avec succès',
+						},
+					});
+				},
+			}
+		);
+	};
+
 	if (errorTrip) {
-		return <ErrorScreen error={errorVehicle} />;
+		return <ErrorScreen error={errorTrip} />;
 	}
 	if (errorDriver) {
-		return <ErrorScreen error={errorTrip} />;
+		return <ErrorScreen error={errorDriver} />;
 	}
 	if (errorVehicle) {
-		return <ErrorScreen error={errorTrip} />;
+		return <ErrorScreen error={errorVehicle} />;
 	}
 
 	if (isLoading || vehicleLoading || driverLoading) {
 		return <LoadingScreen />;
 	}
 
-	// TODO: Avoir un mode inscription et un mode consultation
-
 	return (
 		<ScrollView style={globalStyles.container}>
-			<ReturnButton />
+			<View style={styles.header}>
+				<ReturnButton style={styles.backButton} />
+				<CustomButton
+					text={'Rejoindre le trajet'}
+					textProps={{ color: 'background' }}
+					backgroundColor={'secondary'}
+					buttonStyle={styles.joinButton}
+					onPress={handleSubscribe}
+				/>
+			</View>
 			<ThemedText
 				type='header3'
-				style={styles.title}
+				style={detailTripStyles.title}
 			>
 				{trips &&
 					new Date(trips.datetime).toLocaleDateString('fr-FR', {
@@ -77,21 +121,21 @@ const DetailedTripPage = () => {
 			</ThemedText>
 			<ThemedText
 				type='header5'
-				style={styles.title}
+				style={detailTripStyles.title}
 			>
 				{start && end && `${start.name} -> ${end.name}`}
 			</ThemedText>
-			<View style={styles.labelContainer}>
+			<View style={detailTripStyles.labelContainer}>
 				<TripLabel status={trips?.status ?? 'upcoming'} />
 			</View>
-			<View style={styles.body}>
-				<View style={styles.mapContainer}>
+			<View style={detailTripStyles.body}>
+				<View style={detailTripStyles.mapContainer}>
 					{start && end && trips?.points && trips?.points?.length > 0 && (
 						<RouteMap
 							start={start}
 							end={end}
 							waypoints={checkpoints ?? []}
-							style={styles.map}
+							style={detailTripStyles.map}
 						/>
 					)}
 					{Boolean(
@@ -110,39 +154,39 @@ const DetailedTripPage = () => {
 						/>
 					)}
 				</View>
-				<View style={styles.description}>
-					<View style={styles.userContainer}>
+				<View style={detailTripStyles.description}>
+					<View style={detailTripStyles.userContainer}>
 						<Image
 							source={require('@/assets/images/user-picture.jpg')}
-							style={styles.userImage}
+							style={detailTripStyles.userImage}
 						/>
 						{trips?.driver?.username && (
 							<ThemedText
 								type='header5'
-								style={styles.driverName}
+								style={detailTripStyles.driverName}
 							>
 								{trips?.driver?.username}
 							</ThemedText>
 						)}
 					</View>
-					<View style={styles.descriptionBody}>
+					<View style={detailTripStyles.descriptionBody}>
 						{trips?.description && (
-							<View style={styles.descriptionText}>
+							<View style={detailTripStyles.descriptionText}>
 								<ThemedText type='header6'>Description du trajet</ThemedText>
 								<ThemedText>{trips.description}</ThemedText>
 							</View>
 						)}
-						<View style={styles.stars}>
+						<View style={detailTripStyles.stars}>
 							<ThemedText type={'bigger'}>{driver?.stars}</ThemedText>
 							<Stars rating={driver?.stars as ratingType} />
 						</View>
 					</View>
 				</View>
 
-				<View style={styles.complementaryInfos}>
+				<View style={detailTripStyles.complementaryInfos}>
 					<ThemedText
 						type={'header5'}
-						style={styles.titleInfos}
+						style={detailTripStyles.titleInfos}
 						color={'background'}
 					>
 						Infos complémentaires
@@ -163,102 +207,26 @@ const DetailedTripPage = () => {
 								` (${getHoursAndMinutes(point.waitingTime)})`}
 						</ThemedText>
 					))}
-					{/*	Liste des points du trajet + temps d'attente */}
-					{/*	 Temps de voyage */}
 				</View>
 			</View>
 		</ScrollView>
 	);
 };
 
+export default DetailedTripPage;
+
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		marginTop: 25,
-		backgroundColor: Colors.light.background,
-		display: 'flex',
-		width: '85%',
-		marginHorizontal: 'auto',
-		height: '100%',
-	},
-	title: {
-		textAlign: 'center',
-		marginBottom: 5,
-	},
-	body: {
-		gap: 20,
-	},
-	labelContainer: {
-		width: '90%',
-		marginTop: 10,
-		marginBottom: 30,
-	},
-	mapContainer: {
-		display: 'flex',
-		gap: 12,
-		height: '40%',
-	},
-	map: {
-		borderWidth: 1,
-		borderColor: Colors.light.text,
-		flex: 1,
-		// maxHeight: '50%',
-	},
-	description: {
-		backgroundColor: Colors.light.accent + '9F', // modifie l'opacité
+	header: {
 		display: 'flex',
 		flexDirection: 'row',
 		justifyContent: 'space-between',
 		alignItems: 'center',
-		paddingHorizontal: 15,
-		paddingBottom: 15,
 	},
-	rating: {
-		marginTop: 45,
+	joinButton: {
+		maxWidth: '60%',
+		marginBottom: '5%',
 	},
-	descriptionBody: {
-		width: '  60%',
-		display: 'flex',
-		justifyContent: 'space-between',
-		marginTop: 'auto',
-		marginBottom: '2%',
-		gap: 20,
-	},
-	userContainer: {
-		gap: 3,
-	},
-	userImage: {
-		width: 100,
-		height: 100,
-		borderRadius: 50,
-		marginTop: 20,
-		borderColor: '#FFFA',
-		borderWidth: 2,
-	},
-	descriptionText: {
-		display: 'flex',
-		flexDirection: 'column',
-		justifyContent: 'space-between',
-	},
-	driverName: {
-		textAlign: 'center',
-	},
-	stars: {
-		marginLeft: '3%',
-		marginRight: 'auto',
-		display: 'flex',
-		flexDirection: 'row',
-		gap: '5%',
-		alignItems: 'center',
-	},
-	complementaryInfos: {
-		backgroundColor: Colors.light.primary,
-		padding: '5%',
-	},
-	titleInfos: {
-		textAlign: 'center',
-		marginBottom: '1%',
+	backButton: {
+		paddingVertical: '2%',
 	},
 });
-
-export default DetailedTripPage;
